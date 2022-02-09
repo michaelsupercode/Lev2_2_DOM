ee60fa6063ace7c612b8ffa35c96432ad3afaa91
SuperCoder <supercoder@AirvonSerCoder3.fritz.box>
2022-02-09 20:41:32 +0100

    first com

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let firstName = document.getElementById("vorname");
+let lastName = document.getElementById("nachname");
+let country = document.getElementById("land");
+
+function functionName(formData) {
+    console.log("vorname: " + firstName.value);
+    console.log("nachname: " + lastName.value);
+    console.log("from: " + country.value);
+}
